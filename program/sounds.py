9bce250331ddbd2fc4608cddaae0ec320b333abb
LazyCrazyTom <tclare50@gmail.com>
2022-04-04 15:47:37 +0100

    recreating simple patch

--- program/sounds.py
+++ program/sounds.py
@@ -36,20 +36,33 @@ from notes import notes
 
 ######################################
 
-basic = [notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["G4"],notes["G4"]]
-basic_bass = [notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["G2"],notes["G2"]]
+# basic = [notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["A4"],notes["G4"],notes["G4"]]
+# basic_bass = [notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["A2"],notes["G2"],notes["G2"]]
+
+# s = Server().boot()
+# bpm1 = 60/120
+# asdr = Adsr(attack=.05, decay=0, sustain=1, release=.05, dur=0.15, mul=.5)
+# seq = Sequencer(bpm1, basic).mix(2).out() # can provide with or without envelope
+# #bass = Sequencer(bpm1, basic_bass).mix(2).out()
+# kick = SoundSequencer(bpm1, "sounds/kick.wav", [1,0,1,0]).out()
+# snares = SoundSequencer(bpm1, "sounds/snare.wav", [0,1,0,1]).out()
+# bpm2 = 60/240
+# hihats = SoundSequencer(bpm2, "sounds/hihat.wav", [1,1,1,1,1,1,1,1]).out()
+# bpm3 = 60/520
+# #perc1 = SoundSequencer(bpm3, "sounds/perc1.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
+# #perc2 = SoundSequencer(bpm3, "sounds/perc2.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
+# #perc3 = SoundSequencer(bpm3, "sounds/perc3.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
+# s.gui(locals())
+
+########################################
+## Simple Patch
 
 s = Server().boot()
-bpm1 = 60/120
-asdr = Adsr(attack=.05, decay=0, sustain=1, release=.05, dur=0.15, mul=.5)
-seq = Sequencer(bpm1, basic).mix(2).out() # can provide with or without envelope
-#bass = Sequencer(bpm1, basic_bass, mul=0.2).mix(2).out()
-#kick = SoundSequencer(bpm1, "sounds/kick.wav", [1,0,1,0]).out()
-#snares = SoundSequencer(bpm1, "sounds/snare.wav", [0,1,0,1]).out()
-bpm2 = 60/240
-#hihats = SoundSequencer(bpm2, "sounds/hihat.wav", [1,1,1,1,1,1,1,1]).out()
-bpm3 = 60/520
-#perc1 = SoundSequencer(bpm3, "sounds/perc1.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
-#perc2 = SoundSequencer(bpm3, "sounds/perc2.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
-#perc3 = SoundSequencer(bpm3, "sounds/perc3.wav", [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).out()
+bpm1 = 60/100
+kicks = SoundSequencer(bpm1, "sounds/lofi1.wav", [1,1,1,1]).out()
+bpm2 = 60/200
+hihats = SoundSequencer(bpm2, "sounds/hihat.wav", [1,1,1,1,1,1,1,1]).out()
+
+## uh, problem...the hihat and kick are becoming out of time, after like 7 or 8 bars. No clue why, but it isn't the length of the sample
+## hopefully it's something to do with the bpm?
 s.gui(locals())
